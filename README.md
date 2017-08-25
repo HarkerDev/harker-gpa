dadac3a6399a497517669243361524590ac21827
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-08-24 23:21:20 -0700

    Update structure

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # harker-gpa :pencil2:
-a gpa calculator for students at the harker school. harker gpa is based on a 4.8 scale.
+A GPA calculator for students at The Harker School. Harker GPA is weighed on a 4.8 scale.
 
-checkout the calculator at http://tiny.cc/gpa or https://n3a9.github.io/harker-gpa/
+Checkout the calculator at http://tiny.cc/gpa.
 
-## how the gpa is calculated
+## How the GPA is calculated
 <img width="850" alt="screen shot 2017-05-31 at 10 09 20 pm" src="https://cloud.githubusercontent.com/assets/7104017/26665372/ff76b4a8-464d-11e7-9daa-ea95f3fd882c.png">
--- script.js
+++ script.js
@@ -1,50 +1,34 @@
 function submitForm() {
+    var totalSum = 0;
+    var totalClasses = 7;
 
-	/*
-		var weightedIndex = document.forms[0].uww.selectedIndex;
-		var weighted=true;
-		if (weightedIndex == 0) {
-			weighted=false;
-		}
-	*/
+    for (i = 1; i < 8; i++) {
+        var classs = document.getElementById("class" + i);
+        if ((i == 6 || i == 7) && (classs.children[0].selectedIndex == 2)) {
+            totalClasses--;
+            continue;
+        }
+        totalSum += value(classs.children[0].selectedIndex, classs.children[1].selectedIndex);
+    }
 
-	var totalSum = 0;
-	var totalClasses = 7;
+    var gpa = round(totalSum / totalClasses, 2);
+    var div = document.getElementById("gpa");
 
-	for (i = 1; i < 8; i++) {
-		var classs = document.getElementById("class" + i);
-		if ((i == 6 || i == 7) && (classs.children[0].selectedIndex == 2)) {
-			totalClasses--;
-			continue;
-		}
-		totalSum += value(classs.children[0].selectedIndex, classs.children[1].selectedIndex);
-		/*
-		if (!weighted) {
-			totalSum += value(false, classs.children[1].selectedIndex);
-		} else {
-			totalSum += value(classs.children[0].selectedIndex, classs.children[1].selectedIndex);
-		}
-		*/
-	}
-
-	var gpa = round(totalSum / totalClasses, 2);
-	var div = document.getElementById("gpa");
-
-	div.innerHTML = "Your Harker GPA is " + gpa;
+    div.innerHTML = "Your Harker GPA is " + gpa;
 }
 
 function value(honors, gradeIndex) {
-	var values = [4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0];
-	var value = 0;
-	if (honors == 1) {
-		value += 0.5;
-	}
-	value += values[gradeIndex];
+    var values = [4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0];
+    var value = 0;
+    if (honors == 1) {
+        value += 0.5;
+    }
+    value += values[gradeIndex];
     console.log(value);
-	return value;
+    return value;
 }
 
 function round(value, precision) {
-	var multiplier = Math.pow(10, precision || 0);
-	return Math.round(value * multiplier) / multiplier;
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
 }
